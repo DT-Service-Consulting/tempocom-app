f1a1545529e70c652199bd63789195e53f7b682c
Husain <mohammedhussin1998@gmail.com>
2025-06-05 15:21:38 +0200

    Goood

--- pages/Delay_Calculator.py
+++ pages/Delay_Calculator.py
@@ -69,12 +69,13 @@ with col3:
     st.markdown("#### Departure Delays")
     heatmap.load_and_prepare()
     heatmap.filter_and_prepare_heatmap()
-    fig_dep = heatmap.render_heatmap()
-    st.pyplot(fig_dep)
+    fig_dep = heatmap.render_heatmap()  # Must return a matplotlib Figure
+    st.plotly_chart(fig_dep)  
+
 
 with col4:
     st.markdown("#### Arrival Delays")
     heatmap.load_and_prepare1()
     heatmap.filter_and_prepare_heatmap1()
     fig_arr = heatmap.render_heatmap1()
-    st.pyplot(fig_arr)
+    st.plotly_chart(fig_arr) 
